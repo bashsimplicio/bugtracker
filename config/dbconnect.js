64bfd7b55b852fb8dc9d0a4bc276657e17f1d6d9
Bash Simplicio <basillefelizsimplicio@gmail.com>
2020-05-17 17:10:08 +0800

    resolved conflict

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -13,20 +13,7 @@ pool.connect(err => {
     console.error('connection error', err.stack)
   } else {
     console.log('connected')
-    console.log('port', pool.port)
   }
 })
 
 module.exports={pool}
-
-// var { Client } = require('pg')
-
-// var client = new Client({
-//     host:process.env.PG_HOST,
-//     port:process.env.PG_PORT,
-//     database: process.env.PG_DB_NAME,
-//     user:process.env.PG_USER,
-//     password:process.env.PG_PASSWORD,
-// })
-
-// module.exports={client}
